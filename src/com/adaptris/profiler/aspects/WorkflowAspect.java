cde7235f199e236bc8d3d575a975be9f21c2f39d
Ashley Anderson <ashley.anderson@adaptris.com>
2014-07-07 17:00:45 +0100

    NTERLOK-93 : Refactor/Remove/Update
    After removing much code, I have tidied up the class to provide
    more/different data to the New Relic plugin. This provides the plugin
    with more random data, which can then showcase the plugins capabilities
    a little better.

--- src/com/adaptris/profiler/aspects/WorkflowAspect.java
+++ src/com/adaptris/profiler/aspects/WorkflowAspect.java
@@ -1,5 +1,6 @@
 package com.adaptris.profiler.aspects;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,81 +20,86 @@ import com.adaptris.profiler.MessageProcessStep;
 import com.adaptris.profiler.ProcessStep;
 import com.adaptris.profiler.ReflectionHelper;
 import com.adaptris.profiler.StepType;
-import com.adaptris.profiler.client.ClientFactory;
 import com.adaptris.profiler.client.EventReceiver;
+import com.adaptris.profiler.client.MessageStepIncrementor;
 import com.adaptris.profiler.client.PluginFactory;
 
 @Aspect
 public class WorkflowAspect {
 
   protected transient Log log = LogFactory.getLog(this.getClass().getName());
-  
+
   private static Map<String, ProcessStep> waitingForCompletion = new HashMap<String, ProcessStep>();
-  
+
   @Before("call(void workflowStart(com.adaptris.core.AdaptrisMessage))  && within(com.adaptris.core..*)")
   public synchronized void beforeService(JoinPoint jp) {
     try {
       DefaultSerializableMessageTranslator translator = new DefaultSerializableMessageTranslator();
-      
+
       String workflowClass = jp.getTarget().getClass().getSimpleName();
-      AdaptrisMessage message = ((AdaptrisMessage) jp.getArgs()[0]);
+      AdaptrisMessage message = (AdaptrisMessage) jp.getArgs()[0];
       String uniqueId = ReflectionHelper.getUniqueId(jp.getTarget());
       String messageId = message.getUniqueId();
-      
+
       MessageProcessStep step = new MessageProcessStep();
       step.setMessageId(messageId);
       step.setStepInstanceId(uniqueId);
-      step.setStepName(workflowClass);
+      /*
+       * generate a suffix that distinguishes steps (for our example),
+       * and serves as a process duration too
+       */
+      long suffix = (long)( Math.random() * 10000);
+      step.setStepName(workflowClass + "-" + (int) (suffix / 1000));
       step.setStepType(StepType.WORKFLOW);
-      step.setOrder(ClientFactory.getInstance().getStepIncrementor().generate(messageId));
-      step.setTimeTakenMs(new Date().getTime());
-      step.setTimeStarted(new Date().getTime());
-      message.addMetadata("ENTRY_TIMESTAMP", new Date().toGMTString());
+      step.setOrder(new MessageStepIncrementor().generate(messageId));
+      step.setTimeTakenMs(suffix);
+      Date now = new Date();
+      step.setTimeStarted(now.getTime());
+      message.addMetadata("ENTRY_TIMESTAMP", new SimpleDateFormat("d MMM yyyy HH:mm:ss 'Z'").format(now));
       step.setMessage(translator.translate(message));
-      
+
       String key = messageId + workflowClass + uniqueId;
       waitingForCompletion.put(key, step);
-      
+
       AdaptrisMessageConsumer consumer = ((WorkflowImp) jp.getTarget()).getConsumer();
       MessageProcessStep consumerStep = new MessageProcessStep();
       consumerStep.setMessageId(messageId);
       consumerStep.setStepInstanceId(consumer.getUniqueId());
       consumerStep.setStepName(consumer.getClass().getSimpleName());
       consumerStep.setStepType(StepType.CONSUMER);
-      consumerStep.setOrder(ClientFactory.getInstance().getStepIncrementor().generate(messageId));
+      consumerStep.setOrder(new MessageStepIncrementor().generate(messageId));
       consumerStep.setMessage(translator.translate(message));
-      consumerStep.setTimeTakenMs(0l);
-            
+      consumerStep.setTimeTakenMs(suffix);
+
       this.sendEvent(consumerStep);
-      
+
       log.debug("BEFORE WORKFLOW (" + workflowClass + "(" + uniqueId + "): " + messageId);
     } catch (Exception e) {
       log.error(e);
-    } 
+    }
   }
-  
+
   @After("call(void workflowEnd(com.adaptris.core.AdaptrisMessage, com.adaptris.core.AdaptrisMessage))  && within(com.adaptris.core..*)")
   public synchronized void afterService(JoinPoint jp) {
     try {
       String workflowClass = jp.getTarget().getClass().getSimpleName();
       String uniqueId = ReflectionHelper.getUniqueId(jp.getTarget());
       String messageId = ((AdaptrisMessage) jp.getArgs()[0]).getUniqueId();
-    
+
       String key = messageId + workflowClass + uniqueId;
       ProcessStep step = waitingForCompletion.get(key);
-            
-      long startTime = step.getTimeTakenMs();
-      step.setTimeTakenMs(new Date().getTime() - startTime);
-      
+
+      //step.setTimeTakenMs(new Date().getTime() - step.getTimeStarted());
+
       waitingForCompletion.remove(key);
       this.sendEvent(step);
-      
+
       log.debug("AFTER WORKFLOW (" + workflowClass + "(" + uniqueId + "): " + messageId);
     } catch (Exception e) {
       log.error(e);
-    } 
+    }
   }
-  
+
   private void sendEvent(ProcessStep step) throws Exception {
     for(EventReceiver receiver : PluginFactory.getInstance().getReceivers()) {
       receiver.onEvent(step);
