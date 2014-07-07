fb0e7dace55bad99e923a8fb918880ad610bbbc3
Ashley Anderson <ashley.anderson@adaptris.com>
2014-07-07 16:59:03 +0100

    INTERLOK-93 : Remove unnecessary code
    This is part of the refactor after removing a lot of unneeded code.

--- src/com/adaptris/profiler/aspects/ProducerAspect.java
+++ src/com/adaptris/profiler/aspects/ProducerAspect.java
@@ -17,8 +17,8 @@ import com.adaptris.profiler.MessageProcessStep;
 import com.adaptris.profiler.ProcessStep;
 import com.adaptris.profiler.ReflectionHelper;
 import com.adaptris.profiler.StepType;
-import com.adaptris.profiler.client.ClientFactory;
 import com.adaptris.profiler.client.EventReceiver;
+import com.adaptris.profiler.client.MessageStepIncrementor;
 import com.adaptris.profiler.client.PluginFactory;
 
 @Aspect
@@ -32,52 +32,52 @@ public class ProducerAspect {
   public synchronized void beforeService(JoinPoint jp) {
     try {
       DefaultSerializableMessageTranslator translator = new DefaultSerializableMessageTranslator();
-      
+
       String producerClass = jp.getTarget().getClass().getSimpleName();
-      AdaptrisMessage message = ((AdaptrisMessage) jp.getArgs()[0]);
+      AdaptrisMessage message = (AdaptrisMessage) jp.getArgs()[0];
       String uniqueId = ReflectionHelper.getUniqueId(jp.getTarget());
       String messageId = message.getUniqueId();
-    
+
       MessageProcessStep step = new MessageProcessStep();
       step.setMessageId(messageId);
       step.setStepInstanceId(uniqueId);
       step.setStepName(producerClass);
       step.setStepType(StepType.PRODUCER);
-      step.setOrder(ClientFactory.getInstance().getStepIncrementor().generate(messageId));
+      step.setOrder(new MessageStepIncrementor().generate(messageId));
       step.setTimeTakenMs(new Date().getTime());
       step.setMessage(translator.translate(message));
-      
+
       String key = messageId + producerClass + uniqueId;
       waitingForCompletion.put(key, step);
-      
+
       log.debug("BEFORE PRODUCE (" + producerClass + "(" + uniqueId + "): " + messageId);
     } catch (Exception e) {
       log.error(e);
-    } 
+    }
   }
-  
+
   @After("call(void produce(com.adaptris.core.AdaptrisMessage, com.adaptris.core.ProduceDestination))  && within(com.adaptris.core..*)")
   public synchronized void afterService(JoinPoint jp) {
     try {
       String producerClass = jp.getTarget().getClass().getSimpleName();
       String uniqueId = ReflectionHelper.getUniqueId(jp.getTarget());
       String messageId = ((AdaptrisMessage) jp.getArgs()[0]).getUniqueId();
-    
+
       String key = messageId + producerClass + uniqueId;
       ProcessStep step = waitingForCompletion.get(key);
-      
+
       long startTime = step.getTimeTakenMs();
       step.setTimeTakenMs(new Date().getTime() - startTime);
-      
+
       waitingForCompletion.remove(key);
       this.sendEvent(step);
-      
+
       log.debug("AFTER PRODUCE (" + producerClass + "(" + uniqueId + "): " + messageId);
     } catch (Exception e) {
       log.error(e);
-    } 
+    }
   }
-  
+
   private void sendEvent(ProcessStep step) throws Exception {
     for(EventReceiver receiver : PluginFactory.getInstance().getReceivers()) {
       receiver.onEvent(step);
