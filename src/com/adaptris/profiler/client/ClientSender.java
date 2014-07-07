ae933627904b1f9899c7667a2b722fca154eab08
Ashley Anderson <ashley.anderson@adaptris.com>
2014-07-07 16:55:13 +0100

    INTERLOK-93 : Remove unnecessary code
    On the advice of Aaron, I have gutted much of the profiler project;
    what's left is all that's necessary to get the New Relic plugin working.

--- src/com/adaptris/profiler/client/ClientSender.java
+++
@@ -1,9 +0,0 @@
-package com.adaptris.profiler.client;
-
-import com.adaptris.profiler.ProcessStep;
-
-public interface ClientSender {
-
-  public void sendProcessStep(ProcessStep step) throws Exception;
-  
-}
--- src/com/adaptris/profiler/client/StepTimer.java
+++
@@ -1,8 +0,0 @@
-package com.adaptris.profiler.client;
-
-public interface StepTimer {
-
-  public void startTime(String messageId, String stepName, String stepId);
-  
-  public long getTime(String messageId, String stepName, String stepId);
-}
