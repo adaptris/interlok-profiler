faab5c24bff57de182591b8bb0e03577afdf00d2
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2016-10-31 12:23:32 +0000

    INTERLOK-1197 We now test to make sure the aspect only handles requests from type Workflow.

--- src/com/adaptris/profiler/aspects/WorkflowAspect.java
+++ src/com/adaptris/profiler/aspects/WorkflowAspect.java
@@ -12,6 +12,7 @@ import org.aspectj.lang.annotation.Before;
 
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.SerializableAdaptrisMessage;
+import com.adaptris.core.Workflow;
 import com.adaptris.core.WorkflowImp;
 import com.adaptris.profiler.MessageProcessStep;
 import com.adaptris.profiler.ProcessStep;
@@ -25,15 +26,18 @@ public class WorkflowAspect extends BaseAspect {
   @Before("call(void workflowStart(com.adaptris.core.AdaptrisMessage)) && within(com.adaptris..*)")
   public synchronized void beforeService(JoinPoint jp) {
     try {
-      AdaptrisMessage message = (AdaptrisMessage) jp.getArgs()[0];
-      Date now = new Date();
-      message.addMetadata("AdaptrisWorkflowEntryTimestamp", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
-      SerializableAdaptrisMessage serializedMsg = serialize(message);
-      sendEvent(createStep(StepType.CONSUMER, ((WorkflowImp) jp.getTarget()).getConsumer(), serializedMsg));
-      MessageProcessStep workflowStep = createStep(StepType.WORKFLOW, jp.getTarget(), serializedMsg);
-      workflowStep.setTimeStarted(now.getTime());
-      waitingForCompletion.put(generateStepKey(jp), workflowStep);
-      log("Before Workflow", jp);
+      if(jp.getTarget() instanceof Workflow) {
+        AdaptrisMessage message = (AdaptrisMessage) jp.getArgs()[0];
+        Date now = new Date();
+        message.addMetadata("AdaptrisWorkflowEntryTimestamp", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
+        SerializableAdaptrisMessage serializedMsg = serialize(message);
+        
+        sendEvent(createStep(StepType.CONSUMER, ((WorkflowImp) jp.getTarget()).getConsumer(), serializedMsg));
+        MessageProcessStep workflowStep = createStep(StepType.WORKFLOW, jp.getTarget(), serializedMsg);
+        workflowStep.setTimeStarted(now.getTime());
+        waitingForCompletion.put(generateStepKey(jp), workflowStep);
+        log("Before Workflow", jp);
+      }
     }
     catch (Exception e) {
       log.error("", e);
@@ -42,15 +46,17 @@ public class WorkflowAspect extends BaseAspect {
 
   @After("call(void workflowEnd(com.adaptris.core.AdaptrisMessage, com.adaptris.core.AdaptrisMessage)) && within(com.adaptris..*)")
   public synchronized void afterService(JoinPoint jp) {
-    String key = generateStepKey(jp);
-    ProcessStep step = waitingForCompletion.get(key);
-    // Step will only be null, if we've had an error in the beforeService (serializing the message).
-    if (step != null) {
-      long difference = System.currentTimeMillis() - step.getTimeStarted();
-      step.setTimeTakenMs(difference);
-      waitingForCompletion.remove(key);
-      this.sendEvent(step);
-      log("After Workflow", jp);
+    if(jp.getTarget() instanceof Workflow) {
+      String key = generateStepKey(jp);
+      ProcessStep step = waitingForCompletion.get(key);
+      // Step will only be null, if we've had an error in the beforeService (serializing the message).
+      if (step != null) {
+        long difference = System.currentTimeMillis() - step.getTimeStarted();
+        step.setTimeTakenMs(difference);
+        waitingForCompletion.remove(key);
+        this.sendEvent(step);
+        log("After Workflow", jp);
+      }
     }
   }
 
