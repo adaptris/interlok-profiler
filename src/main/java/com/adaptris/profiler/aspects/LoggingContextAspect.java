f69ebd6b39e3c95f170fa035420c4b4397bc1b9f
Lewin Chan <lewin.chan@adaptris.com>
2020-02-25 11:53:06 +0000

    INTERLOK-3190 Add LoggingContext aspect
    - Add specific profiler for the logging-context
    - Add a profiler that does all of the interlok aspects.

---
+++ src/main/java/com/adaptris/profiler/aspects/LoggingContextAspect.java
@@ -0,0 +1,46 @@
+/*
+    Copyright 2015 Adaptris Ltd.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package com.adaptris.profiler.aspects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.MDC;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreConstants;
+
+@Aspect
+public class LoggingContextAspect {
+
+  public static final String MESSAGE_ID_CONTEXT = "messageId";
+  public static final String PARENT_ID_CONTEXT = "parentMessageId";
+  
+  @Before("(call(void doService(com.adaptris.core.AdaptrisMessage)) "
+      + "|| call(void onAdaptrisMessage(com.adaptris.core.AdaptrisMessage)) "
+      + "|| call(void onAdaptrisMessage(com.adaptris.core.AdaptrisMessage, java.util.function.Consumer))) "
+      + "&& within(com.adaptris..*)")
+  public synchronized void beforeService(JoinPoint jp) {
+    AdaptrisMessage msg = (AdaptrisMessage) jp.getArgs()[0];
+    String msgId = msg.getUniqueId();
+    String parentId = msg.getMetadataValue(CoreConstants.PARENT_UNIQUE_ID_KEY);    
+    MDC.put(MESSAGE_ID_CONTEXT, msgId);
+    if (StringUtils.isNotEmpty(parentId)) {
+      MDC.put(PARENT_ID_CONTEXT, parentId);
+    }    
+  }
+}
